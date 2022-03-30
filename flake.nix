b94c1c27728d3627c988866987c43c58b65f61b8
David Pätzel <david.paetzel@posteo.de>
2022-03-30 08:42:59 +0200

    ❄ Add (incomplete) defaultPackage, remove pymc dep

--- flake.nix
+++ flake.nix
@@ -2,32 +2,49 @@
   inputs = {
     nixpkgs.url =
       "github:nixos/nixpkgs/7f9b6e2babf232412682c09e57ed666d8f84ac2d";
-    overlays.url = "github:dpaetzel/overlays";
   };
 
-  outputs = { self, nixpkgs, overlays }:
+  outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs {
         inherit system;
-        # That overlay contains arviz.
-        overlays = [ overlays.pymc4 ];
       };
       python = pkgs.python39;
     in {
       # TODO In order to provide a proper default flake here we need to package
       # pystan/httpstan properly.
+      defaultPackage.x86_64-linux = python.pkgs.buildPythonPackage rec {
+        pname = "cmpbayes";
+        version = "0.0.1-beta";
+
+        src = self;
+
+        # We use pyproject.toml.
+        format = "pyproject";
+
+        propagatedBuildInputs = with python.pkgs; [
+            numpy
+            arviz
+            pandas
+        ];
+
+        meta = with pkgs.lib; {
+          description =
+            "Small Python library for Bayesian data analysis for algorithms results";
+          license = licenses.gpl3;
+        };
+      };
 
       devShell.${system} = pkgs.mkShell {
 
         # We use ugly venvShellHook here because packaging pystan/httpstan is
         # not entirely straightforward.
         buildInputs = with pkgs;
-          with python3Packages; [
+          with python.pkgs; [
             python
             venvShellHook
             numpy
-            pymc4
             pandas
           ];
 
